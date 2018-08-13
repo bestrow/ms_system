d3e44f88ef9dd493437881c9814223408eca8523
bestrow <1142465640@qq.com>
2018-08-13 23:17:53 +0800

    Ehcache和Spring代码测试

---
+++ EhcacheSpring/src/test/java/com/example/test/SpringTestCase.java
@@ -0,0 +1,11 @@
+package com.example.test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations = {"classpath:spring-base.xml","classpath:spring-ehcache.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringTestCase extends AbstractJUnit4SpringContextTests{
+
+}
